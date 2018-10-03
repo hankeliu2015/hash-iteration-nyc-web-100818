ee0678fb03311ff8dd0bff09944ff20d6340d8cb
Henry <hanke.liu@gmail.com>
2018-10-03 15:59:57 -0400

    passed with some errors

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,22 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+#require 'pry'
+
+passengers = {
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
+suite_e: "Crumpet the Elf"
+}
+#binding.pry
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+
+
 end
+
+  winner
